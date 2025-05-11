f664c848b933194dd57b2285f2da05002005b16e
grahame21 <143662036+grahame21@users.noreply.github.com>
2025-05-12 03:16:31 +0930

    Update trainfinder_login.py

--- trainfinder_login.py
+++ trainfinder_login.py
@@ -1,48 +1,41 @@
 import os
 import time
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Load secrets from GitHub Actions environment
+# Load secrets
 username = os.getenv("TRAINFINDER_USERNAME")
 password = os.getenv("TRAINFINDER_PASSWORD")
 proxy_user = os.getenv("PROXYMESH_USERNAME")
 proxy_pass = os.getenv("PROXYMESH_PASSWORD")
 
-# Set proxy server
+# ProxyMesh config
 proxy = f"http://{proxy_user}:{proxy_pass}@au.proxymesh.com:31280"
 
-# Configure headless Chrome
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument(f"--proxy-server={proxy}")
+options = uc.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument(f'--proxy-server={proxy}')
 
-# Launch browser
-driver = webdriver.Chrome(options=chrome_options)
+driver = uc.Chrome(options=options, headless=True)
 
 try:
     driver.get("https://trainfinder.otenko.com/home/nextlevel")
     time.sleep(3)
 
     try:
-        # Wait for login form
         username_field = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.ID, "Username"))
         )
         username_field.send_keys(username)
         driver.find_element(By.ID, "Password").send_keys(password)
         driver.find_element(By.ID, "Password").send_keys(Keys.RETURN)
-
         time.sleep(5)
 
-        # Grab cookie
         cookies = driver.get_cookies()
         for cookie in cookies:
             if cookie['name'] == '.ASPXAUTH':
@@ -58,14 +51,11 @@ try:
         driver.save_screenshot("login_fail.png")
         with open("login_debug.html", "w", encoding="utf-8") as f:
             f.write(driver.page_source)
-
-        # Auto-commit debug files to GitHub repo
         subprocess.run("git config user.name github-actions", shell=True)
         subprocess.run("git config user.email actions@github.com", shell=True)
         subprocess.run("git add login_fail.png login_debug.html", shell=True)
         subprocess.run("git commit -m 'Auto debug files from failed login'", shell=True)
         subprocess.run("git push", shell=True)
-
         raise e
 
 finally:
